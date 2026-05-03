fdc6846a33ba469e24d62e797017138c3b774037
Rodrigo Naray <rodrigo.naray@fing.edu.uy>
2026-05-03 20:12:46 -0300

    feat: configurar app.module con todos los módulos y agregar swagger en producto controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { ProductoModule } from '@modules/modules/producto/producto.module';
-import { PedidoModule } from '@modules/modules/pedido/pedido.module';
-import { AuthModule } from '@modules/modules/auth/auth.module';
-import { AdministradorModule } from '@modules/modules/administrador/administrador.module';
-import { ConfiguracionTiendaModule } from '@modules/modules/configuracion/configuracion-tienda.module';
-import { NotificacionModule } from '@modules/modules/notificacion/notificacion.module';
+import { ProductoModule } from '@modules/producto/producto.module';
+import { PedidoModule } from '@modules/pedido/pedido.module';
+import { AuthModule } from '@modules/auth/auth.module';
+import { AdministradorModule } from '@modules/administrador/administrador.module';
+import { ConfiguracionTiendaModule } from '@modules/configuracion/configuracion-tienda.module';
+import { NotificacionModule } from '@modules/notificacion/notificacion.module';
 import { DatabaseModule } from '@common/config/database/database.module';
 
 @Module({
